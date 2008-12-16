ae009650be8ff54a238e8715a8b28dc5e40250d7
Riccardo Schmid <rschmid@wren.krl.caltech.edu>
2008-12-15 18:08:40 -0800

    test

--- test.C
+++ test.C
@@ -1,9 +1,20 @@
-//#include "BSegment.h"
+#include "BSegment.h"
 
 //#include <stdlib.h>
 #include <iostream.h>
 
 int main (int argc, char **argv) {
   cout << "ok\n";
+  BSegment *seg = new BSegment();
+  float *temp = seg->field(1,2,3);
+  cout << temp[0] << "\t"
+       << temp[1] << "\t"
+       << temp[2] << "\n";
+  if (seg->is_valid()) {
+    cout << "valid\n";
+  }
+  else {
+    cout << "not valid\n";
+  }
   return 0;
 }
