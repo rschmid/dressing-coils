5d9ad6d789f92e873eaa5551ac52738ffc6d31e2
Riccardo Schmid <rschmid@wren.krl.caltech.edu>
2008-12-15 17:56:29 -0800

    Compiles

--- BSegment.C
+++ BSegment.C
@@ -1,51 +1,35 @@
 #include "BSegment.h"
 
+float BSegment::_temp_field[3];
+
 BSegment::BSegment() {
-  this->_default_constructor();
+  _default_constructor();
 }
 
 BSegment::BSegment(BSegment *clone) {
-  this->_default_constructor();
+  _default_constructor();
+  if (clone->p1) {
+    p1 = new float(3);
+    p1[0] = clone->p1[0];
+    p1[1] = clone->p1[1];
+    p1[2] = clone->p1[2];
+  }
+  if (clone->p2) {
+    p2 = new float(3);
+    p2[0] = clone->p2[0];
+    p2[1] = clone->p2[1];
+    p2[2] = clone->p2[2];
+  }
 }
 
 BSegment::~BSegment() {
-  if (!_prev) {
-    delete _p1;
-  }
-  else {
-    _prev->_next = 0;
-  }
-  if (!_next) {
-    delete _p2;
-  }
-  else {
-    _next->prev = 0;
-  }
+  delete p1;
+  delete p2;
 }
 
 void BSegment::_default_constructor() {
-  this->_prev = 0;
-  this->_next = 0;
-  this->_p1 = 0;
-  this->_p2 = 0;
-}
-
-/*
- * Returns 1 if completed successfully
- */
-void SetPoints(const double *p1, const double *p2) {
-  if (p1) {
-    if (!_p1) _p1 = double(3);
-    _p1[0] = p1[0];
-    _p1[1] = p1[1];
-    _p1[2] = p1[2];
-  }
-  if (p2) {
-    if (!_p2) _p2 = double(3);
-    _p2[0] = p2[0];
-    _p2[1] = p2[1];
-    _p2[2] = p2[2];
-  }
+  p1 = 0;
+  p2 = 0;
 }
 
 BSegment *BSegment::Clone() {
@@ -53,6 +37,14 @@ BSegment *BSegment::Clone() {
   return clone;
 }
 
-int is_valid() {
-  return (_p1 && _p2);
+int BSegment::is_valid() {
+  return (p1 && p2);
+}
+
+float *BSegment::field(float x, float y, float z) {
+  float *temp = _temp_field;
+  temp[0] = x;
+  temp[1] = y;
+  temp[2] = z;
+  return temp ;
 }
--- BSegment.h
+++ BSegment.h
@@ -2,26 +2,20 @@
 #define __BSEGMENT_H
 
 class BSegment {
-
  public:
-
   BSegment();
   BSegment(BSegment *clone);
-  int SetPoints(const double *p1, const double *p2);
-  double *GetPoint1() {return _p1;}
-  double *GetPoint2() {return _p2;}
-  BSegment *prev() {return _prev;}
-  BSegment *next() {return _next;}
+  ~BSegment();
   BSegment *Clone();
   int is_valid();
-
+  float *field(float x, float y, float z);
+  // Members
+  float *p1;
+  float *p2;
  private:
+  static float _temp_field[3];
   void _default_constructor();
-  BSegment *_prev;
-  BSegment *_next;
-  double *_p1;
-  double *_p2;
-}
+};
 
 
 #endif
